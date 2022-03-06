d9a19aac455a2197e13d71ad93bf9f1de0ea6749
KoheiMori <nitorifactoryracing7711@gmail.com>
2022-03-06 20:13:55 +0900

    set up bootstrap4 cdn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  #ルートurlはpostsコントローラーのindexを呼んでください
+  root to: "posts#index"
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
